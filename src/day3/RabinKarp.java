8968d9452b8e406fcbecc8d1d76e0189e86765e9
한대영 <dyhan@sk.com>
2019-10-25 17:34:45 +0900

    라빈칼프 (Rabin-Karp) 알고리즘 2차 시도 (String.substring 제거, 자체 Hash 함수) - 좀 더 돌지만 시간초과

--- src/day3/RabinKarp.java
+++ src/day3/RabinKarp.java
@@ -13,15 +13,26 @@ import java.util.*;
  *    2-2 : 해시값이 동일하면 실제 문자열 비교 후 성공 시 반환
  * 3. 실패 반환
  *
- * Java hashCode() 사용 버전
- * - String.substring() 사용 -> 타임아웃 될 것으로 예상
+ * 1차 시도 (시간초과)
+ *   - Java hashCode() 사용 버전
+ *   - String.substring() 사용 -> 타임아웃 될 것으로 예상
+ *
+ * 2차 시도 (1차시도보다 더 돌지만 시간초과)
+ *   - string.substring 제거 필요 (new 제거)
+ *   - 그러려면 새로운 hash 함수 필요
  */
 public class RabinKarp {
-    static int hashCode(String in) {
-        return in.hashCode();
+    static int getHash(String input, int start, int length) {
+        final int base = 256;
+        int hashVal = 0;
+        for (int i=0; i<length; i++) {
+            hashVal = hashVal * base + input.charAt(start + i);
+        }
+
+        return hashVal;
     }
 
-    static int getRabinKarp(String string, String pattern) {
+    static int getRabinKarp01(String string, String pattern) {
         int p_hash = pattern.hashCode();
 
         for (int i=0; i <= string.length() - pattern.length(); i++) {
@@ -34,13 +45,28 @@ public class RabinKarp {
         return 0;
     }
 
+    static int getRabinKarp02(String string, String pattern) {
+        int p_hash = getHash(pattern, 0, pattern.length());
+        int s_hash;
+
+        for (int i=0; i <= string.length() - pattern.length(); i++) {
+            s_hash = getHash(string, i, pattern.length());
+            if (p_hash == s_hash) {
+                if (pattern.equals(string.substring(i, i + pattern.length())))
+                    return 1;
+            }
+        }
+
+        return 0;
+    }
+
     public static void main(String[] args) {
-//        System.out.println(getRabinKarp("Sealover", "ver"));
+//        System.out.println(getRabinKarp02("Sealover", "ver"));
 
         Scanner sc = new Scanner(System.in);
         String string = sc.nextLine();
         String pattern = sc.nextLine();
 
-        System.out.println(getRabinKarp(string, pattern));
+        System.out.println(getRabinKarp02(string, pattern));
     }
 }

92fdf183f1e9978243866171a92e8bb900604ad1
한대영 <dyhan@sk.com>
2019-10-28 17:18:39 +0900

    2902 - KMP 문제라는데 걍 쉽게 대문자영역으로 크기비교해서 풀어버림

--- src/day3/Q02_2902_KMP.java
+++ src/day3/Q02_2902_KMP.java
@@ -6,8 +6,15 @@ public class Q02_2902_KMP {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
-        System.out.println(input);
 
+        StringBuffer stringBuffer = new StringBuffer();
 
+        for (int i=0; i<input.length(); i++) {
+            if (input.charAt(i) >= 'A' && input.charAt(i) <= 'Z') {
+                stringBuffer.append(input.charAt(i));
+            }
+        }
+
+        System.out.println(stringBuffer.toString());
     }
 }
